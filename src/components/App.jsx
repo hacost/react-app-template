1d54e332cb2ebe4f481ebbefceafd71b23613518
Héctor Acosta <hacost@hotmail.com>
2021-02-01 18:30:02 -0600

    install and config SASS

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../assets/styles/App.scss'
 
 const App = () => (
      <h1>Hello app</h1> 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -25,6 +26,14 @@ module.exports = {
           loader: 'html-loader',
         },
       },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          'css-loader',
+          'sass-loader',
+        ],
+      }, 
     ],
   },
   plugins: [
@@ -32,5 +41,8 @@ module.exports = {
       template: './public/index.html',
       filename: './index.html',
     }),
+    new MiniCssExtractPlugin({
+        filename: 'assets/[name].css',
+      }),
   ],
 };
