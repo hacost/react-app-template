38f10242173205c5e0989ab0856cd86801b67623
Héctor Acosta <hacost@hotmail.com>
2021-02-13 16:34:50 -0600

    add use of static images with webpack

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import '../assets/styles/components/Header.scss';
+import logo from '../assets/static/your-logo.png';
 
 const Header = () => (
   <header className='item header'>
+    <img className='header__logo' src={logo} alt='your logo' />
     <h1>Header component</h1>
   </header>
 );
--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,16 @@ module.exports = {
           'css-loader',
           'sass-loader',
         ],
-      }, 
+      },
+      {
+        test: /\.(png|gif|jpg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: { name: 'assets/[hash].[ext]' },
+          },
+        ],
+      },
     ],
   },
   plugins: [
