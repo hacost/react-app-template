514c7d3a67c68e3bb95c5761df94601c6d9ffb79
Héctor Acosta <hacost@hotmail.com>
2021-02-13 13:05:52 -0600

    add inline source map to webpak.config

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
