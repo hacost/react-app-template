06d6da77c0be8a0c1fd610fef8227c61b01194d9
Héctor Acosta <hacost@hotmail.com>
2021-02-13 18:19:32 -0600

    add Roboto font via Google Web Fonts

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,6 +4,7 @@ import '../assets/styles/components/Content.scss';
 const Content = () => (
   <section className='item content'>
     <h2>Content component </h2>
+    <p>All your content is here...</p>
   </section>
 );
 
